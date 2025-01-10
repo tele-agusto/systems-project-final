4e03108748fa6b880c75c58482e8120efe04415b
tele-agusto <121642752+tele-agusto@users.noreply.github.com>
2025-01-10 16:59:35 +0000

    Update config.py

--- GNN/config.py
+++ GNN/config.py
@@ -8,15 +8,6 @@ class Config:
     DATA_FOLDER = './simulated_data/'  # path to the folder data will be written to and read from
     RESULT_FOLDER = './'  # path to the folder data will be written to and read from
 
-    # SIMULATED DATA OPTIONS
-    CONNECTOME_MEAN = 0.0  # mean of the distribution from which connectomes will be sampled
-    CONNECTOME_STD = 1.0  # std of the distribution from which connectomes will be sampled
-    SCORE_MEAN = 90.0  # mean of the distribution from which scores will be sampled
-    SCORE_STD = 10.0  # std of the distribution from which scores will be sampled
-    N_SUBJECTS = 30  # number of subjects in the simulated data
-    ROI = 116  # number of regions of interest in brain graph
-    SPD = True  # whether or not to make generated matrices symmetric positive definite
-
     # EVALUATION OPTIONS
     K_FOLDS = 3  # number of cross validation folds
 
@@ -27,14 +18,6 @@ class Config:
         WD = 5e-4  # weight decay
         DROPOUT = 0.1  # dropout rate
 
-    # PNA OPTIONS
-    class PNA:
-        NUM_EPOCH = 100  # number of epochs the process will be run for
-        LR = 1e-4  # learning rate
-        WD = 5e-4  # weight decay
-        DROPOUT = 0.1  # dropout rate
-        SCALERS = ["identity", "amplification", "attenuation"]  # scalers used by PNA
-        AGGRS = ['sum', 'mean', 'var', 'max']  # aggregators used by PNA
 
     class SampleSelection:
         #changed from True to False
